42f4158d235660c98eb51684b61f7a6fdf6e53af
akane0915 <asialkane@gmail.com>
2017-05-29 20:04:50 -0700

    create test project to work through setting up an ember project

--- config/environment.js
+++ config/environment.js
@@ -17,6 +17,13 @@ module.exports = function(environment) {
       }
     },
 
+    firebase: {
+      apiKey: "AIzaSyCYojH5AnsvCKu6xNyoHKeX0sfg1gOgeCc",
+      authDomain: "super-rentals-6b55f.firebaseapp.com",
+      databaseURL: "https://super-rentals-6b55f.firebaseio.com",
+      storageBucket: "super-rentals-6b55f.appspot.com"
+    },
+
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
